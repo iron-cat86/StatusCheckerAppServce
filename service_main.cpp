408f0b5fce9dfa7d69a18af5b141c2dc0ce7fd22
Belova.AP <belova.ap@gorizont-vs.ru>
2025-12-24 03:44:31 +0300

    ctrl+c for service

--- service_main.cpp
+++ service_main.cpp
@@ -1,10 +1,33 @@
 #include <QCoreApplication>
+#include <QDebug>
 #include "simpleserver.h"
+#include <csignal>
+
+QCoreApplication *app_ptr = nullptr;
+
+void handle_sigint(int signal) {
+    if (signal == SIGINT) {
+        qDebug() << "Received SIGINT (Ctrl+C). Initiating graceful shutdown...";
+        if (app_ptr) {
+            app_ptr->quit();
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    app_ptr = &a;
+
+    std::signal(SIGINT, handle_sigint);
+
     SimpleServer server;
-    server.startServer(8080); // Запускаем на порту 8080
-    return a.exec();
+    server.startServer(8080);
+
+    qDebug() << "Service is running. Press Ctrl+C to stop gracefully.";
+
+    int returnCode = a.exec();
+
+    qDebug() << "Service shutdown complete.";
+    return returnCode;
 }
